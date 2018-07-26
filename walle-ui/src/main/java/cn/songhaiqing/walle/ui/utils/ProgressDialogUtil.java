e315fd46955e132055181d7f068075b292001f98
Harlan-PC\Harlan <651193340@qq.com>
2018-07-26 16:27:10 +0800

    Add "ProgressDialogUtil"

---
+++ walle-ui/src/main/java/cn/songhaiqing/walle/ui/utils/ProgressDialogUtil.java
@@ -0,0 +1,25 @@
+package cn.songhaiqing.walle.ui.utils;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+public class ProgressDialogUtil {
+    private static ProgressDialog  progressDialog;
+
+    public static void show(Context context,int messageId){
+        if(progressDialog != null && progressDialog.isShowing()){
+            progressDialog.dismiss();
+        }
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage(context.getString(messageId));
+        progressDialog.show();
+    }
+
+    public static void dimiss(){
+        if(progressDialog == null || !progressDialog.isShowing()){
+            return;
+        }
+        progressDialog.dismiss();
+        progressDialog = null;
+    }
+}
